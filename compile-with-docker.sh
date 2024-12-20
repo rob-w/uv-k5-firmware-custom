174a5869324f6e577f96124ac5806488fc7c87b4
Robert Woerle <rwoerle@mibtec.de>
2024-12-20 11:32:18 +0100

    enable latest spectrum, disable many others
    Signed-off-by: Robert Woerle <rwoerle@mibtec.de>

--- compile-with-docker.sh
+++ compile-with-docker.sh
@@ -58,12 +58,19 @@ voxless() {
         ENABLE_VOX=0 \
         ENABLE_AIRCOPY=0 \
         ENABLE_AUDIO_BAR=0 \
-        ENABLE_FEAT_F4HWN_SPECTRUM=0 \
+        ENABLE_BIG_FREQ=0 \
+        ENABLE_SMALL_BOLD=0 \
         ENABLE_NOAA=0 \
+        ENABLE_KEEP_MEM_NAME=0 \
+        ENABLE_NO_CODE_SCAN_TIMEOUT=0 \
+        ENABLE_FEAT_F4HWN_RX_TX_TIMER=0 \
         ENABLE_FEAT_F4HWN_RESTORE_SCAN=0 \
         ENABLE_FEAT_F4HWN_CHARGING_C=0 \
         ENABLE_FEAT_F4HWN_NARROWER=0 \
         ENABLE_FEAT_F4HWN_RESCUE_OPS=0 \
+        ENABLE_FEAT_F4HWN_RX_TX_TIMER=0 \
+        ENABLE_FEAT_F4HWN_CONTRAST=0 \
+        ENABLE_FEAT_F4HWN_CA=0 \
         EDITION_STRING=Voxless \
         TARGET=f4hwn.voxless \
         && cp f4hwn.voxless* compiled-firmware/"
